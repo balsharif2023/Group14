e30760c5201e93b0436ebe66aa8cb92ab6319518
Glitch (financial-insights) <none>
2023-11-06 18:41:44 +0000

    🚌🙎 Checkpoint ./login.php:61857770/914 ./includes/dbh.inc.php:61857770/266

---
+++ includes/dbh.inc.php
@@ -0,0 +1,7 @@
+<?php
+  $servername="https://financial-insights.glitch.me";
+  $dBUsername="root";
+  $dBPassword="";
+  $dBName="financialadvising";
+
+  $conn = ""
--- login.php
+++ login.php
@@ -3,11 +3,11 @@ include_once 'header.html';
 ?>
 <section class="signup-form">
   <h2>Log In</h2>
-  <form action="signup.inc.php" method="post">
-    <input type="text" name="email" placeholder="Email...">
-    <input type="text" name="uid" placeholder="Username...">
-    <input type="password" name="pwd" placeholder="Password...">
-    <input type="password" name="pwd repeat" placeholder="Repeat password...">
-    <button type="submit" name="submit">Sign Up</button>  
+  <div class="signup-form-form">
+    <input type="text" name="uid" placeholder="Username/Email">
+    <input type="password" name="pwd" placeholder="Password">
+    <button type="submit" name="submit">Log In</button>  
   </form>
+  </div>
+
 </section>
