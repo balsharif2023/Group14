bcd26fc215b74a58be6a71fcfd52e90b4c56cf08
Glitch (financial-insights) <none>
2023-11-07 00:41:42 +0000

    🎩👾 Checkpoint ./index.html:61857770/16787 ./plaid.html:61857770/6287 ./login.html:61857770/15009 ./contact.html:61857770/12504 ./about.html:61857770/11069 ./src/vite.config.js:61857770/722 ./src/main.jsx:61857770/466 ./src/index.jsx:61857770/988 ./src/App.jsx:61857770/4253 ./src/pages/about.jsx:61857770/5202 ./src/pages/home.jsx:61857770/5475 ./src/js/bootstrap.bundle.js:61857770/435815

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.Fragment>
-		<App />
-	</React.Fragment>
+  <React.Fragment>
+    <App />
+  </React.Fragment>
 );
