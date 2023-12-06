93554665dbb6acd1a9f2e371f6e4a4f3ad423603
Glitch (financial-insights) <none>
2023-12-06 07:25:06 +0000

    🐿🏨 Checkpoint ./.glitch-assets:61857770/1367 ./src/login/script.js:61857770/392

--- src/login/script.js
+++ src/login/script.js
@@ -82,8 +82,7 @@ submitButton.addEventListener("click", function() {
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
-      console.log("Success! Welcome back!");
-      window.alert("Success! Welcome back!");
+      window.location.assign('https://financial-insights.glitch.me/')
       // ...
     })
     .catch((error) => {
