5c89f9de2ced4933cd0836995889ee84bf0038ba
Glitch (financial-insights) <none>
2023-12-11 21:18:46 +0000

    🏟🍮 Checkpoint ./signin.html:61857770/14 ./src/login/script.js:61857770/10013

--- src/login/script.js
+++ src/login/script.js
@@ -1,6 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCSTmzYfSzvHu-yavkMfaHzV0R35JVtlzA",
@@ -9,7 +13,7 @@ const firebaseConfig = {
   storageBucket: "financial-insight-975c5.appspot.com",
   messagingSenderId: "657911226160",
   appId: "1:657911226160:web:5af2bb58f42608247c72f5",
-  measurementId: "G-DSPHXPQ336"
+  measurementId: "G-DSPHXPQ336",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,8 +26,8 @@ const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const main = document.getElementById("main");
 const createacct = document.getElementById("create-acct");
-const resetpasswordbutton = document.getElementById("reset-password-bttn");
-const resetform = document.getElementById("reset-password");
+const resetpasswordbutton = document.getElementById("reset");
+const reset = document.getElementById("reset-password");
 
 const resetpassword = document.getElementById("email-reset");
 const resetbutton = document.getElementById("reset-bttn");
@@ -31,75 +35,86 @@ const resetbutton = document.getElementById("reset-bttn");
 const signupEmailIn = document.getElementById("email-signup");
 const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
 const signupPasswordIn = document.getElementById("password-signup");
-const confirmSignUpPasswordIn = document.getElementById("confirm-password-signup");
+const confirmSignUpPasswordIn = document.getElementById(
+  "confirm-password-signup"
+);
 const createacctbtn = document.getElementById("create-acct-btn");
 
 const returnBtn = document.getElementById("return-btn");
 
-var email, password, signupEmail, signupPassword, confirmSignupEmail, confirmSignUpPassword,resetPasswordEmail;
+var email,
+  password,
+  signupEmail,
+  signupPassword,
+  confirmSignupEmail,
+  confirmSignUpPassword,
+  resetPasswordEmail;
 
-createacctbtn.addEventListener("click", function() {
+//on createacct page
+createacctbtn.addEventListener("click", function () {
   var isVerified = true;
 
   signupEmail = signupEmailIn.value;
   confirmSignupEmail = confirmSignupEmailIn.value;
-  if(signupEmail != confirmSignupEmail) {
-      window.alert("Email fields do not match. Try again.")
-      isVerified = false;
+  if (signupEmail != confirmSignupEmail) {
+    window.alert("Email fields do not match. Try again.");
+    isVerified = false;
   }
 
   signupPassword = signupPasswordIn.value;
   confirmSignUpPassword = confirmSignUpPasswordIn.value;
-  if(signupPassword != confirmSignUpPassword) {
-      window.alert("Password fields do not match. Try again.")
-      isVerified = false;
+  if (signupPassword != confirmSignUpPassword) {
+    window.alert("Password fields do not match. Try again.");
+    isVerified = false;
   }
-  
-  if(signupEmail == null || confirmSignupEmail == null || signupPassword == null || confirmSignUpPassword == null) {
+
+  if (
+    signupEmail == null ||
+    confirmSignupEmail == null ||
+    signupPassword == null ||
+    confirmSignUpPassword == null
+  ) {
     window.alert("Please fill out all required fields.");
     isVerified = false;
-  }  
-  if(isVerified) {
+  }
+  if (isVerified) {
     createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
       .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      // ...
-      window.alert("Success! Account created.");
-      
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-      window.alert("Error occurred. Try again.");
-    });
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        window.alert("Success! Account created.");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        window.alert("Error occurred. Try again.");
+      });
   }
 });
 
-resetbutton.addEventListener("click", function() {
+//reset password
+
+resetbutton.addEventListener("click", function () {
   var isVerified = true;
-  
+
   resetPasswordEmail = resetpassword.value;
-  
-  if(resetPasswordEmail == null)
-  {
+
+  if (resetPasswordEmail == null) {
     window.alert("Please fill out all required fields.");
     isVerified = false;
+  } else {
   }
-  else
-    {
-      
-    }
-  resetpasswordbutton.addEventListener("click", function() {
+  resetpasswordbutton.addEventListener("click", function () {
     var isverified = true;
     main.style.display = "none";
-    resetform.style.display = "block";
-    
-});
+    reset.style.display = "block";
+  });
 });
 
-submitButton.addEventListener("click", function() {
+//submit button on main
+submitButton.addEventListener("click", function () {
   email = emailInput.value;
   console.log(email);
   password = passwordInput.value;
@@ -109,7 +124,9 @@ submitButton.addEventListener("click", function() {
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
-      window.location.assign('https://65777176294e9440bad967c4--musical-nasturtium-e6f447.netlify.app/')
+      window.location.assign(
+        "https://65777176294e9440bad967c4--musical-nasturtium-e6f447.netlify.app/"
+      );
       // ...
     })
     .catch((error) => {
@@ -120,15 +137,19 @@ submitButton.addEventListener("click", function() {
     });
 });
 
-
-
-signupButton.addEventListener("click", function() {
-    main.style.display = "none";
-    createacct.style.display = "block";
+//reset button on main
+resetpasswordbutton.addEventListener("click", function () {
+  main.style.display = "block";
 });
 
+//sign up on main
+signupButton.addEventListener("click", function () {
+  main.style.display = "none";
+  createacct.style.display = "block";
+});
 
-returnBtn.addEventListener("click", function() {
-    main.style.display = "block";
-    createacct.style.display = "none";
+//return on createacct
+returnBtn.addEventListener("click", function () {
+  main.style.display = "block";
+  createacct.style.display = "none";
 });
