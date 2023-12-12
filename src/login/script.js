20abbfd602613ec5e4cbde756a2d4e8e01cad49d
Glitch (financial-insights) <none>
2023-12-12 02:09:40 +0000

    💇🌌 Checkpoint ./src/login/script.js:61857770/13626 ./signin.html:61857770/816 ./src/login/reset.css:61857770/1152 ./src/login/main.css:61857770/147

--- src/login/script.js
+++ src/login/script.js
@@ -4,7 +4,7 @@ import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
-  sendPasswordResetEmail
+  sendPasswordResetEmail,
 } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
 const firebaseConfig = {
@@ -36,7 +36,9 @@ const resetbutton = document.getElementById("reset-bttn");
 const signupEmailIn = document.getElementById("email-signup");
 const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
 const signupPasswordIn = document.getElementById("password-signup");
-const confirmSignUpPasswordIn = document.getElementById("confirm-password-signup");
+const confirmSignUpPasswordIn = document.getElementById(
+  "confirm-password-signup"
+);
 const createacctbtn = document.getElementById("create-acct-btn");
 
 const returnBtn = document.getElementById("return-btn");
@@ -77,6 +79,7 @@ createacctbtn.addEventListener("click", function () {
     window.alert("Please fill out all required fields.");
     isVerified = false;
   }
+
   if (isVerified) {
     createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
       .then((userCredential) => {
@@ -97,24 +100,25 @@ createacctbtn.addEventListener("click", function () {
 //reset password
 
 resetbutton.addEventListener("click", function () {
-  main.style.display = "block";
-  createacct.style.display = "block";
-  reset.style.display = "none";
-  
   var isVerified = true;
 
   resetPasswordEmail = resetpassword.value;
-  
-app.auth().sendPasswordResetEmail(resetPasswordEmail).then(() => {
-  console.log('email sent!');
-}).catch(function(error) {
-  // An error happened.
-});
+
+  app.sendPasswordResetEmail(resetPasswordEmail)
+    .then(() => {
+      console.log("email sent!");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Error occurred. Try again.");
+      window.alert("Error occurred. Try again.");
+    });
 });
 
 //submit button on main
 
-submitButton.addEventListener("click", function() {
+submitButton.addEventListener("click", function () {
   email = emailInput.value;
   console.log(email);
   password = passwordInput.value;
@@ -136,65 +140,26 @@ submitButton.addEventListener("click", function() {
     });
 });
 
-
-signupButton.addEventListener("click", function() {
-    main.style.display = "none";
-    createacct.style.display = "block";
+signupButton.addEventListener("click", function () {
+  main.style.display = "none";
+  createacct.style.display = "block";
+  reset.style.display = 
 });
 
-returnBtn.addEventListener("click", function() {
-    main.style.display = "block";
-    createacct.style.display = "none";
+returnBtn.addEventListener("click", function () {
+  main.style.display = "block";
+  createacct.style.display = "none";
+  
 });
 
 //reset button on main
 resetpasswordbutton.addEventListener("click", function () {
   main.style.display = "none";
-  createacct.style.display = "block";
+  createacct.style.display = "none";
   reset.style.display = "block";
 });
 
 returnBtn2.addEventListener("click", function () {
   main.style.display = "block";
   createacct.style.display = "block";
-  reset.style.display = "none";
-});
-
-createacctbtn.addEventListener("click", function() {
-  var isVerified = true;
-
-  signupEmail = signupEmailIn.value;
-  confirmSignupEmail = confirmSignupEmailIn.value;
-  if(signupEmail != confirmSignupEmail) {
-      window.alert("Email fields do not match. Try again.")
-      isVerified = false;
-  }
-
-  signupPassword = signupPasswordIn.value;
-  confirmSignUpPassword = confirmSignUpPasswordIn.value;
-  if(signupPassword != confirmSignUpPassword) {
-      window.alert("Password fields do not match. Try again.")
-      isVerified = false;
-  }
-  
-  if(signupEmail == null || confirmSignupEmail == null || signupPassword == null || confirmSignUpPassword == null) {
-    window.alert("Please fill out all required fields.");
-    isVerified = false;
-  }
-  
-  if(isVerified) {
-    createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
-      .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      // ...
-      window.alert("Success! Account created.");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-      window.alert("Error occurred. Try again.");
-    });
-  }
 });
